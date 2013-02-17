a844faa91544f8340352afe0ec1ecd20f27d08f2
Balazs Faludi <balazsfaludi@gmail.com>
2013-02-17 16:54:59 +0100

    Added option to change the relative size of the first and last tab.

--- BFTabCircle/BFTabCircle.m
+++ BFTabCircle/BFTabCircle.m
@@ -13,6 +13,8 @@
 @interface BFTabCircle ()
 
 //@property (nonatomic) NSArray *buttons;
+@property (nonatomic) CGFloat extraInnerAngle; // Gives the first and last tabs a little mit more space.
+@property (nonatomic) CGFloat extraOuterAngle; // Gives the first and last tabs a little mit more space.
 
 @end
 
@@ -29,6 +31,8 @@
 		_outerRadius = 150.0f;
 		_innerRadius = 30.0f;
 		_verticalOffset = 16.f;
+		_extraInnerAngle = 0.6f;
+		_extraOuterAngle = -0.2f;
 		self.items = items;
 		[self updateFrame];
 		self.backgroundColor = [UIColor clearColor];
@@ -116,20 +120,27 @@
 	CGPoint outerEnd	= CGPointMake(self.bounds.size.width / 2.0f + outerIntersectionX, self.outerRadius + self.verticalOffset);
 	CGPoint innerStart	= CGPointMake(self.bounds.size.width / 2.0f - innerIntersectionX, self.outerRadius + self.verticalOffset);
 	CGPoint innerEnd	= CGPointMake(self.bounds.size.width / 2.0f + innerIntersectionX, self.outerRadius + self.verticalOffset);
+
+	
+	CGFloat additionalOuterAngle = atanf((outerStart.y - center.y) / (center.x - outerStart.x)) - self.extraOuterAngle;
+	CGFloat totalOuterAngle = M_PI + 2.0f * additionalOuterAngle;
+	CGFloat startOuterAngle = M_PI + additionalOuterAngle;
+	CGFloat outerAnglePiece = totalOuterAngle / self.items.count;
+	
+	CGFloat additionalInnerAngle = atanf((innerStart.y - center.y) / (center.x - innerStart.x)) - self.extraInnerAngle;
+	CGFloat totalInnerAngle = M_PI + 2.0f * additionalInnerAngle;
+	CGFloat startInnerAngle = M_PI + additionalInnerAngle;
+	CGFloat innerAnglePiece = totalInnerAngle / self.items.count;
+
 	
 	// Calculate the bottom left and top left edges. Handle the first tab differently.
 	if (index == 0) {
 		points[0] = innerStart;
 		points[1] = outerStart;
 	} else {
-		CGFloat additionalAngle = atanf((outerStart.y - center.y) / (center.x - outerStart.x));
-		CGFloat totalOuterAngle = M_PI + 2.0f * additionalAngle;
-		CGFloat startOuterAngle = M_PI + additionalAngle;
-		CGFloat outerAngle = startOuterAngle - totalOuterAngle / self.items.count * index;
-	
-		CGFloat totalInnerAngle = (M_PI / self.items.count) * index;
-		points[0] = CGPointMake(self.outerRadius - self.innerRadius * cosf(totalInnerAngle),
-								self.outerRadius - self.innerRadius * sinf(totalInnerAngle));
+		CGFloat outerAngle = startOuterAngle - outerAnglePiece * (index);
+		CGFloat innerAngle = startInnerAngle - innerAnglePiece * (index);
+		points[0] = [BFCircleGeometry pointOnCircleWithCenter:center radius:self.innerRadius atAngle:2.0f * M_PI - innerAngle];
 		points[1] = [BFCircleGeometry pointOnCircleWithCenter:center radius:self.outerRadius atAngle:2.0f * M_PI - outerAngle];
 	}
 	
@@ -138,15 +149,9 @@
 		points[2] = outerEnd;
 		points[3] = innerEnd;
 	} else {
-		
-		CGFloat additionalAngle = atanf((outerStart.y - center.y) / (center.x - outerStart.x));
-		CGFloat totalOuterAngle = M_PI + 2.0f * additionalAngle;
-		CGFloat startOuterAngle = M_PI + additionalAngle;
-		CGFloat outerAngle = startOuterAngle - totalOuterAngle / self.items.count * (index + 1);
-		
-		CGFloat totalInnerAngle = (M_PI / self.items.count) * (index + 1);
-		points[3] = CGPointMake(self.outerRadius - self.innerRadius * cosf(totalInnerAngle),
-								self.outerRadius - self.innerRadius * sinf(totalInnerAngle));
+		CGFloat outerAngle = startOuterAngle - outerAnglePiece * (index + 1);
+		CGFloat innerAngle = startInnerAngle - innerAnglePiece * (index + 1);
+		points[3] = [BFCircleGeometry pointOnCircleWithCenter:center radius:self.innerRadius atAngle:2.0f * M_PI - innerAngle];
 		points[2] = [BFCircleGeometry pointOnCircleWithCenter:center radius:self.outerRadius atAngle:2.0f * M_PI - outerAngle];
 	}
 }
